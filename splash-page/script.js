9e9393fbbdf97ea15e979e7a10817a4fa28d4431
Iván Villegas <haivan_cva@gmail.com>
2020-09-30 13:03:48 -0500

    paint

---
+++ splash-page/script.js
@@ -0,0 +1,30 @@
+const { body } = document;
+
+const changeBackground = (background) => {
+  let prevBackground;
+
+  if (body.className) {
+    prevBackground = body.className;
+  }
+
+  body.className = "";
+  switch (background) {
+    case "1":
+      return prevBackground === "background-1"
+        ? false
+        : body.classList.add("background-1");
+
+    case "2":
+      return prevBackground === "background-2"
+        ? false
+        : body.classList.add("background-2");
+
+    case "3":
+      return prevBackground === "background-3"
+        ? false
+        : body.classList.add("background-3");
+
+    default:
+      break;
+  }
+};
